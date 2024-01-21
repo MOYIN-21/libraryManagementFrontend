4691ad434ba4be888d0bb758682fe27730d8e799
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-01-21 21:53:07 +0100

    first commit

---
+++ WEBSITE/home.js
@@ -0,0 +1,11 @@
+// submitLoginForm(){
+//     const userName = document.getElementById('name-id').value
+//     const password = document.getElementById('password-id').value
+
+//     const formData ={
+//         userName-id: userName,
+//         password-id: password
+
+//     } 
+
+// }
